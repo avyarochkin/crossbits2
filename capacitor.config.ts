3ab52b2fcd35cd2c39cf425d860b31299b3de18d
Alexey Yarochkin <alexey.yarochkin@gmail.com>
2024-07-22 14:15:23 +0200

    Removed ionic-cordova config. Updated ionic/angular-toolkit, removed tslint and added browserlist. Updated cocoapods.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli'
 
 const config: CapacitorConfig = {
     appId: 'com.braintrain.crossbits2',
-    appName: 'crossbits2',
+    appName: 'CrossBits',
     webDir: 'www',
     server: {
         androidScheme: 'https'
